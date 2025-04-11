ce3dd50be73b3da80a7dc65fbc6212bfd2503b2f
tranthekyky <ttk8701ad@gmail.com>
2025-04-11 21:36:17 +0700

    WareHouse

--- src/main/java/org/example/electricstore/service/impl/InventoryTransactionService.java
+++ src/main/java/org/example/electricstore/service/impl/InventoryTransactionService.java
@@ -47,68 +47,88 @@ public class InventoryTransactionService implements IInventoryTransactionService
     }
 
     @Override
-    public void importToWarehouse(ProductChoiceRequestDTO productChoiceRequestDTO) {
-        List<ProductChoiceDTO> productChoiceDTOList = productChoiceRequestDTO.getProducts();
-
-        for (ProductChoiceDTO productChoiceDTO : productChoiceDTOList) {
-            Product product = this.productRepository.findById(productChoiceDTO.getProductId()).orElse(null);
-            WareHouse wareHouse = this.wareHouseRepository.findByProduct(product);
-            if (wareHouse != null) {
-                wareHouse.setQuantity(wareHouse.getQuantity() + productChoiceDTO.getProductQuantity());
-                wareHouse.setPrice(productChoiceDTO.getProductPrice());
-                this.wareHouseRepository.save(wareHouse);
-            } else {
-                this.wareHouseRepository.save(WareHouse.builder()
-                        .product(product)
-                        .quantity(productChoiceDTO.getProductQuantity())
-                        .price(productChoiceDTO.getProductPrice())
-                        .build());
-            }
-        }
-        InventoryTransaction inventoryTransaction = this.transactionMapper.convertToInventoryTransactionImport(productChoiceRequestDTO);
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
-        Employee employee = this.employeeRepository.findEmployeeByUser_Username(username);
-        if (employee != null) {
-            inventoryTransaction.setEmployee(employee);
-        }
-        this.inventoryTransactionRepository.save(inventoryTransaction);
-    }
-
-    @Override
     public void exportFromWarehouse(ProductChoiceRequestDTO productChoiceRequestDTO) {
-        List<ProductChoiceDTO> productChoiceDTOList = productChoiceRequestDTO.getProducts();
-        for (ProductChoiceDTO productChoiceDTO : productChoiceDTOList) {
-            Product product = this.productRepository.findById(productChoiceDTO.getProductId()).orElse(null);
-            WareHouse wareHouse = this.wareHouseRepository.findByProduct(product);
-            wareHouse.setQuantity(wareHouse.getQuantity() - productChoiceDTO.getQuantityInput());
-            this.wareHouseRepository.save(wareHouse);
-        }
-        InventoryTransaction inventoryTransaction = this.transactionMapper.convertToInventoryTransactionExport(productChoiceRequestDTO);
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
-        Employee employee = this.employeeRepository.findEmployeeByUser_Username(username);
-        if (employee != null) {
-            inventoryTransaction.setEmployee(employee);
-        }
-        this.inventoryTransactionRepository.save(inventoryTransaction);
+
     }
+
     @Override
-    public Page<InventoryTransaction> searchTransactions(String field, String keyword, int page, int size,
-                                                         LocalDate fromDate, LocalDate toDate, String transactionType) {
-        Pageable pageable1 = PageRequest.of(page - 1, size);
-        if (transactionType != null && !transactionType.isEmpty()) {
-            return this.inventoryTransactionRepository.searchTransactions(field , keyword,
-                    TransactionType.valueOf(transactionType) , fromDate, toDate, pageable1);
-        }
-        return this.inventoryTransactionRepository.searchTransactions(field , keyword ,
-                null , fromDate, toDate, pageable1);
+    public void importToWarehouse(ProductChoiceRequestDTO productChoiceRequestDTO) {
+
+    }
+
+    @Override
+    public Page<InventoryTransaction> searchTransactions(String field, String keyword, int page, int size, LocalDate fromDate, LocalDate toDate, String transactionType) {
+        return null;
     }
 
     @Override
     public List<TransactionsDetailDTO> getTransactionsDetail(Integer id) {
-        InventoryTransaction inventoryTransaction = this.inventoryTransactionRepository.findById(id).orElseThrow(
-                () -> new RuntimeException("Inventory transaction not found"));
-        return this.transactionMapper.convertToTransactionsDetailDTO(inventoryTransaction) ;
+        return List.of();
     }
+//
+//    @Override
+//    public void importToWarehouse(ProductChoiceRequestDTO productChoiceRequestDTO) {
+//        List<ProductChoiceDTO> productChoiceDTOList = productChoiceRequestDTO.getProducts();
+//
+//        for (ProductChoiceDTO productChoiceDTO : productChoiceDTOList) {
+//            Product product = this.productRepository.findById(productChoiceDTO.getProductId()).orElse(null);
+//            WareHouse wareHouse = this.wareHouseRepository.findByProduct(product);
+//            if (wareHouse != null) {
+//                wareHouse.setQuantity(wareHouse.getQuantity() + productChoiceDTO.getProductQuantity());
+//                wareHouse.setPrice(productChoiceDTO.getProductPrice());
+//                this.wareHouseRepository.save(wareHouse);
+//            } else {
+//                this.wareHouseRepository.save(WareHouse.builder()
+//                        .product(product)
+//                        .quantity(productChoiceDTO.getProductQuantity())
+//                        .price(productChoiceDTO.getProductPrice())
+//                        .build());
+//            }
+//        }
+//        InventoryTransaction inventoryTransaction = this.transactionMapper.convertToInventoryTransactionImport(productChoiceRequestDTO);
+//        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+//        Employee employee = this.employeeRepository.findEmployeeByUser_Username(username);
+//        if (employee != null) {
+//            inventoryTransaction.setEmployee(employee);
+//        }
+//        this.inventoryTransactionRepository.save(inventoryTransaction);
+//    }
+//
+//    @Override
+//    public void exportFromWarehouse(ProductChoiceRequestDTO productChoiceRequestDTO) {
+//        List<ProductChoiceDTO> productChoiceDTOList = productChoiceRequestDTO.getProducts();
+//        for (ProductChoiceDTO productChoiceDTO : productChoiceDTOList) {
+//            Product product = this.productRepository.findById(productChoiceDTO.getProductId()).orElse(null);
+//            WareHouse wareHouse = this.wareHouseRepository.findByProduct(product);
+//            wareHouse.setQuantity(wareHouse.getQuantity() - productChoiceDTO.getQuantityInput());
+//            this.wareHouseRepository.save(wareHouse);
+//        }
+//        InventoryTransaction inventoryTransaction = this.transactionMapper.convertToInventoryTransactionExport(productChoiceRequestDTO);
+//        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+//        Employee employee = this.employeeRepository.findEmployeeByUser_Username(username);
+//        if (employee != null) {
+//            inventoryTransaction.setEmployee(employee);
+//        }
+//        this.inventoryTransactionRepository.save(inventoryTransaction);
+//    }
+//    @Override
+//    public Page<InventoryTransaction> searchTransactions(String field, String keyword, int page, int size,
+//                                                         LocalDate fromDate, LocalDate toDate, String transactionType) {
+//        Pageable pageable1 = PageRequest.of(page - 1, size);
+//        if (transactionType != null && !transactionType.isEmpty()) {
+//            return this.inventoryTransactionRepository.searchTransactions(field , keyword,
+//                    TransactionType.valueOf(transactionType) , fromDate, toDate, pageable1);
+//        }
+//        return this.inventoryTransactionRepository.searchTransactions(field , keyword ,
+//                null , fromDate, toDate, pageable1);
+//    }
+//
+//    @Override
+//    public List<TransactionsDetailDTO> getTransactionsDetail(Integer id) {
+//        InventoryTransaction inventoryTransaction = this.inventoryTransactionRepository.findById(id).orElseThrow(
+//                () -> new RuntimeException("Inventory transaction not found"));
+//        return this.transactionMapper.convertToTransactionsDetailDTO(inventoryTransaction) ;
+//    }
 
 
 
--- src/main/java/org/example/electricstore/service/interfaces/IWareHouseService.java
+++ src/main/java/org/example/electricstore/service/interfaces/IWareHouseService.java
@@ -2,10 +2,13 @@ package org.example.electricstore.service.interfaces;
 
 import org.example.electricstore.model.WareHouse;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface IWareHouseService<T> {
-   Page<T> searchWareHouses(String field , String keyword , Integer statusStock ,int page , int size);
+   Page<T> searchWareHouses(LocalDate importDate, String brand, Integer statusStock,
+                            String productCode, String productName, Pageable pageable);
    List<WareHouse> getWareHouses();
 }
